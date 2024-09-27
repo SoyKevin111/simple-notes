0c66ea8ffb88d17c9f63009e2bddfe0ae39bd191
SoyKevin111 <kevinstevennn768xd@gmail.com>
2024-09-26 21:03:36 -0500

    ajus-project: ajustando nombre de las cards y quitando la utilidad del search

--- src/app/components/list-state/list-state.component.ts
+++ src/app/components/list-state/list-state.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
-import { CardComponent } from '../cards/card/card.component';
-import { CardV2Component } from '../cards/card-v2/card-v2.component';
-import { CardV3Component } from '../cards/card-v3/card-v3.component';
+import { CardComponent } from '../cards/cardTodo/card.component';
+import { CardV2Component } from '../cards/cardDoing/card-v2.component';
+import { CardV3Component } from '../cards/cardDone/card-v3.component';
 import { NgClass } from '@angular/common';
 
 @Component({
