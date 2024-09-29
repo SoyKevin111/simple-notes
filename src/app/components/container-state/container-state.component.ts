1a81f51e259ad653bde9d1f53d7eb0a36d6a4ffa
SoyKevin111 <kevinstevennn768xd@gmail.com>
2024-09-29 00:00:43 -0500

    modal-add-card: agregando el modal de flowbite al proyecto.

--- src/app/components/container-state/container-state.component.ts
+++ src/app/components/container-state/container-state.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { ListCardStateComponent } from '../list-card-state/list-card-state.component';
+import { ModalAddCardComponent } from "../../shared/modals/modal-add-card/modal-add-card.component";
 
 @Component({
   selector: 'app-container-state',
   standalone: true,
-  imports: [ListCardStateComponent],
+  imports: [ListCardStateComponent, ModalAddCardComponent],
   templateUrl: './container-state.component.html',
   styleUrl: './container-state.component.scss'
 })
---
+++ src/app/shared/modals/modal-add-card/modal-add-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'modal-add-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-add-card.component.html',
+  styleUrl: './modal-add-card.component.scss'
+})
+export class ModalAddCardComponent {
+
+}
