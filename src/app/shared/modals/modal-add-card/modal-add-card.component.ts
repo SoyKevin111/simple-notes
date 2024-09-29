97cf8e3ecc53029f895cbe739822634ee5bf7f90
SoyKevin111 <kevinstevennn768xd@gmail.com>
2024-09-29 13:43:23 -0500

    moda-add-card: manejando estilos de error del formulario y custom color gray

--- src/app/shared/modals/modal-add-card/modal-add-card.component.ts
+++ src/app/shared/modals/modal-add-card/modal-add-card.component.ts
@@ -1,18 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input } from '@angular/core';
 import { ModalService } from '../modal.service';
+import { FormGroup, ReactiveFormsModule, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'modal-add-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './modal-add-card.component.html',
   styleUrl: './modal-add-card.component.scss'
 })
 export class ModalAddCardComponent {
 
+  //Formulario reactivo
+  addTaskForm: FormGroup;
+  private _fb = inject(FormBuilder);
+
   @Input() modal_addTaskVisible: boolean = false;
   private _modalService = inject(ModalService);
+
+  constructor() {
+    this.addTaskForm = this._fb.group({
+      name: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      state: ['', [Validators.required]]
+    })
+  }
 
 
   closeModal_addTask() {
@@ -20,12 +33,22 @@ export class ModalAddCardComponent {
     this._modalService.close();
   }
 
-
-  onSubmit(event: Event): void {
-    event.preventDefault();
-    this.modal_addTaskVisible = false;
-    this._modalService.close();
-    //servicio para agregar la tarjeta.
+  onSubmit(): void {
+    
+    if (this.addTaskForm.get('state')?.invalid) {
+      this.addTaskForm.get('state')?.markAsTouched();
+    }
+    
+    if (this.addTaskForm.valid) {
+      this.modal_addTaskVisible = false;
+      this._modalService.close();
+      // Servicio para agregar la tarjeta
+      console.log(`name: ${this.addTaskForm.get('name')?.value} - Description: ${this.addTaskForm.get('description')?.value} - State: ${this.addTaskForm.get('state')?.value}`);
+    }
   }
+  
+
+  
+  
 
 }
