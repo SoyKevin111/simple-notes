ab9b4e63fcae6a63b8e809882403216e21d16ec5
SoyKevin111 <kevinstevennn768xd@gmail.com>
2024-09-29 10:43:31 -0500

    modal-add-card: funcionalidad abrir modal con un servicio

--- src/app/components/container-state/container-state.component.ts
+++ src/app/components/container-state/container-state.component.ts
@@ -1,17 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { ListCardStateComponent } from '../list-card-state/list-card-state.component';
 import { ModalAddCardComponent } from "../../shared/modals/modal-add-card/modal-add-card.component";
+import { ModalService } from '../../shared/modals/modal.service';
 
 @Component({
   selector: 'app-container-state',
   standalone: true,
-  imports: [ListCardStateComponent, ModalAddCardComponent],
+  imports: [ListCardStateComponent],
   templateUrl: './container-state.component.html',
   styleUrl: './container-state.component.scss'
 })
 export class ContainerStateComponent {
+
+  //servicio para invocar al modal:
+  private _modalService = inject(ModalService);
   
-  
+  openAddCard(){
+    this._modalService.open(ModalAddCardComponent,{modal_addTaskVisible:true});
+  }
 
 
 }
--- src/app/shared/modals/modal-add-card/modal-add-card.component.ts
+++ src/app/shared/modals/modal-add-card/modal-add-card.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, Input } from '@angular/core';
+import { ModalService } from '../modal.service';
 
 @Component({
   selector: 'modal-add-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './modal-add-card.component.html',
   styleUrl: './modal-add-card.component.scss'
 })
 export class ModalAddCardComponent {
+
+  @Input() modal_addTaskVisible: boolean = false;
+  private _modalService = inject(ModalService);
+
+
+  closeModal_addTask() {
+    this.modal_addTaskVisible = false;
+    this._modalService.close();
+  }
 
 }
