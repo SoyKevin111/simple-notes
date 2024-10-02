9bc0f7001d3bc98c130f544106e2f57d86c56990
SoyKevin111 <kevinstevennn768xd@gmail.com>
2024-10-02 00:38:43 -0500

    toast-function: abstrayendo el codigo del toast para un toast reutilizable, error: el desvanecimiento se nota brusco aveces.

--- src/app/shared/toasts/mocks/toast.mock.ts
+++ src/app/shared/toasts/mocks/toast.mock.ts
@@ -1,13 +1,14 @@
-interface Toast{
+interface Toast {
 	type: string;
 	data: string;
 	path: string;
+	primarycolor: string;
 }
 
-const dataToast:Toast[] = [
-	{type: 'exportTxt', data: 'Project saved to Text.', path: 'assets/img/icon/icon-toast-svg/icon-toast-export.svg'},
-	{type: 'exportJson', data: 'Project saved to Json.', path: 'assets/img/icon/icon-toast-svg/icon-toast-export.svg'},
-	{type:'newTask', data: 'New Task name-card.', path: 'assets/img/icon/icon-toast-svg/icon-toast-export.svg'},
-	{type:'deleteTask', data: 'name-card has been deleted.', path: 'assets/img/icon/icon-toast-svg/icon-toast-export.svg'},
-	{type:'completeTask', data: 'The name-card has been completed.', path: 'assets/img/icon/icon-toast-svg/icon-toast-export.svg'}
+export const dataToast: Toast[] = [
+	{ type: 'exportTxt', data: 'Project saved to Text.', path: 'assets/img/icon/icon-toast-svg/icon-toast-export.svg', primarycolor: '#dcfce7' },
+	{ type: 'exportJson', data: 'Project saved to Json.', path: 'assets/img/icon/icon-toast-svg/icon-toast-export.svg', primarycolor: '#dcfce7' },
+	{ type: 'newTask', data: 'New Task name-card.', path: 'assets/img/icon/icon-toast-svg/icon-toast-export.svg', primarycolor: '' },
+	{ type: 'deleteTask', data: 'name-card has been deleted.', path: 'assets/img/icon/icon-toast-svg/icon-toast-export.svg', primarycolor: '' },
+	{ type: 'completeTask', data: 'The name-card has been completed.', path: 'assets/img/icon/icon-toast-svg/icon-toast-export.svg', primarycolor: '' }
 ];
